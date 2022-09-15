3b5cfaf7ff9531d297a4508381a75a36f1e0f63c
Pierre Etrillard <pierre.etrillard@live.fr>
2022-09-15 17:10:26 +0200

    ajout du module de simulateur d'api pour angular-dev

---
+++ src/app/character-form/character-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Character } from '../model/character.model';
+import { CharacterService } from '../services/character-service';
+
+
+@Component({
+  selector: 'app-character-form',
+  templateUrl: './character-form.component.html',
+  styleUrls: ['./character-form.component.scss']
+})
+export class CharacterFormComponent implements OnInit {
+  @Input() character: Character;
+  alliesList: string[] | undefined;
+  characterDetails: Character | undefined;
+
+  constructor(
+    private characterService: CharacterService,
+    private route: ActivatedRoute,
+    private router:Router) { }
+
+  ngOnInit() {
+    const currentUrl: string = document.location.href;
+    const characterId: string = currentUrl.substring(currentUrl.lastIndexOf("/") + 1);
+    this.characterDetails = this.characterService.getCharacterById(+characterId);
+    
+    this.alliesList = this.characterDetails?.allies
+    console.log(this.alliesList);
+  }
+  hasAllie(allie: string): boolean {
+    return this.character.allies.includes(allie)
+  }
+  selectAllie($event: Event, allie: string) {
+    const isCheked = ($event.target as HTMLInputElement).checked;
+    if (isCheked) {
+      this.character.allies.push(allie);
+    } else {
+      const index = this.character.allies.indexOf(allie);
+      this.character.allies.splice(index, 1)
+    }
+  }
+  onSubmit() {
+console.log('Sub Formulaire/');
+this.router.navigate(['disney',this.character._id])
+
+  }
+}
